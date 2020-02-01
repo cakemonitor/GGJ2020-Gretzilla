a9207224e9c4709be98ac01c020392273a526e7f
Martin <martin.cooke@gmail.com>
2020-02-01 09:58:01 +0000

    Smashable factories

---
+++ Assets/Scripts/DamageArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageArea : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Component[] scripts = other.gameObject.GetComponents<MonoBehaviour>();
+
+        foreach (var script in scripts)
+        {
+            ISmashable smashable = script as ISmashable;
+            if (smashable != null)
+                smashable.Smash();
+        }
+    }    
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject planet;
+    public float planetRadius = 21.0f;
+    public GameObject objectPrefab;
+    public int initialAmount = 5;
+    public float minSpawnTime = 5.0f;
+    public float maxSpawnTime = 15.0f;
+
+    GameObject spawnHelper;
+
+    void Start()
+    {
+        spawnHelper = new GameObject();
+        spawnHelper.name = objectPrefab.name + " spawn helper";
+
+        for (int i = 0; i < initialAmount - 1; i++)
+        {
+            Spawn();
+        }
+
+        TimedSpawn();
+    }
+
+    void Spawn()
+    {
+        spawnHelper.transform.position = planet.transform.position;
+        spawnHelper.transform.rotation = Random.rotation;
+        spawnHelper.transform.Translate(0, planetRadius, 0);
+        
+        Instantiate(objectPrefab, spawnHelper.transform.position, spawnHelper.transform.rotation);
+    }
+
+    void TimedSpawn()
+    {
+        Spawn();
+        float nextSpawn = Random.Range(minSpawnTime, maxSpawnTime);
+        Invoke("TimedSpawn", nextSpawn);
+    }
+}
