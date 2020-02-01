ce4b36b7c10c435c98d95577f9aa07bf9c3be868
harrygood619 <harrygood619@users.noreply.github.com>
2020-02-01 21:28:54 +0000

    Airplane prefab
    Setup an airplane prefab that flies/orbits the earth and is destoryed when colliding with anything. Should be at a height where greta has to jump to hit it so it doesn't collide with buildings

---
+++ Assets/Scripts/Airplane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.parent.transform.Rotate((2 * Time.deltaTime), 0, 0);   
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
