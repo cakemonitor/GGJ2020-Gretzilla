91be48f7a3ba3eaba7aaf2af26061378a8b8d96d
Martin <martin.cooke@gmail.com>
2020-02-01 01:39:18 +0000

    Player-world collision response

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject planet;
     public float moveSpeed = 8.0f;
+    public float turnSpeed = 180.0f;
+    public float attackDuration = 0.25f;
 
     Rigidbody rBody;
     Vector2 motion = Vector2.zero;
-
     Transform centerPivot;
     GameObject movementTarget;
+    float angularMovementSpeed;
+    GameObject damageArea;
+    float planetRadius;
 
     void Start()
     {
@@ -22,14 +26,43 @@ public class PlayerController : MonoBehaviour
         movementTarget = new GameObject();
         movementTarget.name = "Player Movement Target";
         movementTarget.transform.position = transform.position;
+        movementTarget.transform.rotation = transform.rotation;
         movementTarget.transform.SetParent(centerPivot.transform);
+
+        planetRadius = (planet.transform.position - transform.position).magnitude;
+        float circumference = Mathf.PI * 2.0f * planetRadius;
+        angularMovementSpeed = moveSpeed / circumference * 360.0f;
+
+        damageArea = transform.Find("Damage Area").gameObject;
+        DeactiveDamageArea();
     }
 
     void Update()
     {
-        motion.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        motion.y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        centerPivot.transform.LookAt(transform.position, -transform.forward);
+        movementTarget.transform.position = (transform.position - centerPivot.transform.position).normalized * planetRadius + centerPivot.transform.position;
 
-        centerPivot.Rotate(motion.y, motion.x, 0);
+        motion.x = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        motion.y = Input.GetAxis("Vertical") * angularMovementSpeed * Time.deltaTime;
+
+        centerPivot.Rotate(motion.y, 0, motion.x);
+
+        if (Input.GetButtonDown("Attack") && !damageArea.activeSelf)
+        {
+            damageArea.SetActive(true);
+            Invoke("DeactiveDamageArea", attackDuration);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rBody.velocity = Vector3.zero;
+        rBody.MovePosition(movementTarget.transform.position);
+        rBody.MoveRotation(centerPivot.transform.rotation * Quaternion.Euler(90.0f, 0.0f, 0.0f));
+    }
+
+    void DeactiveDamageArea()
+    {
+        damageArea.SetActive(false);
     }
 }
