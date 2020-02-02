b3c0b649bf5be37d33134a10c42d38b79d702067
harrygood619 <harrygood619@users.noreply.github.com>
2020-02-02 11:53:53 +0000

    Start Game Sound
    Added Will's start game sounds with a coroutine to delay the loading

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private IEnumerator Coroutine;
+    public AudioSource StartGameSound;
+
+    public void Awake()
+    {
+        StartGameSound = GetComponent<AudioSource>();
+    }
+
     public void StartGame()
     {
+        StartGameSound.Play(0);
+        Coroutine = WaitThenLoad(25f);
+        StartCoroutine(Coroutine);
+    }
+
+    private IEnumerator WaitThenLoad(float WaitTime)
+    {
+        yield return new WaitForSeconds(WaitTime);
         SceneManager.LoadScene("MovementTest");
     }
 }
