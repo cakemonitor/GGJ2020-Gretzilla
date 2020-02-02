ea9a2f390d4e4dd488ce26dffd301283097f144f
harrygood619 <harrygood619@users.noreply.github.com>
2020-02-02 15:34:26 +0000

    ICBM
    ICBM should go after green energy buildings but if the player is near enough it will retarget after them

--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -15,7 +15,15 @@ public class Factory : MonoBehaviour, ISmashable
 
     public void Smash()
     {
-        Pollution.PollutionBuildings.Remove(gameObject);
+        if (Pollution.PollutionBuildings.Contains(gameObject))
+        {
+            Pollution.PollutionBuildings.Remove(gameObject);
+        }
+        else if (Pollution.CleanBuildings.Contains(gameObject))
+        {
+            Pollution.CleanBuildings.Remove(gameObject);
+        }
+
         GameObject TempExplosion = Instantiate(Explosion, gameObject.transform);
         TempExplosion.transform.localPosition = new Vector3(0, 4f, 0);
         TempExplosion.transform.localScale = new Vector3(7, 7, 7);
--- Assets/Scripts/ICBM.cs
+++ Assets/Scripts/ICBM.cs
@@ -6,16 +6,21 @@ public class ICBM : MonoBehaviour
 {
     public Transform PlayerTransform;
 
+    public GameObject ClosestBuildiung;
+    public GameObject Player;
+
     public float Speed;
     public float TargettingHeight;
+
+    public bool AttackPlayer = false;
 
     void Awake()
     {
         if (PlayerTransform == null)
         {
             PlayerController TempPlayerController = (PlayerController)FindObjectOfType(typeof(PlayerController));
-            GameObject TempPlayer = TempPlayerController.gameObject;
-            PlayerTransform = TempPlayer.transform;
+            Player = TempPlayerController.gameObject;
+            PlayerTransform = Player.transform;
         }
     }
 
@@ -28,14 +33,66 @@ public class ICBM : MonoBehaviour
         }
         else
         {
-            TargetPlayer();
-            Move();
+            if (AttackPlayer == true)
+            {
+                TargetPlayer();
+            }
+            else
+            {
+                TargetBuilding();
+            }
+                Move();
+            
         }
     }
 
     void Move()
     {
         transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+    }
+
+    void TargetBuilding()
+    {
+        if (Pollution.CleanBuildings.Count > 0)
+        {
+            float Distance = 0;
+            float ClosestDistance = 999999;
+
+            for (int i = 0; i < Pollution.CleanBuildings.Count; i++)
+            {
+                if (Pollution.CleanBuildings[i] != null)
+                {
+                    Distance = Vector3.Distance(transform.position, Pollution.CleanBuildings[i].transform.position);
+
+                    if (ClosestDistance >= 999999)
+                    {
+                        ClosestBuildiung = Pollution.CleanBuildings[i];
+                        ClosestDistance = Distance;
+                    }
+                    else
+                    {
+                        if (Distance < ClosestDistance)
+                        {
+                            ClosestBuildiung = Pollution.CleanBuildings[i];
+                            ClosestDistance = Distance;
+                        }
+                    }
+                }
+            }
+
+            // Determine which direction to rotate towards
+            Vector3 TargetDirection = (ClosestBuildiung.transform.position - transform.position);
+            // The step size is equal to speed times frame time.
+            float SingleStep = ((Speed * 0.1f) * Time.deltaTime);
+            // Rotate the forward vector towards the target direction by one step
+            Vector3 Target = Vector3.RotateTowards(transform.forward, TargetDirection, SingleStep, 0.0f);
+            // Calculate a rotation a step closer to the target and applies rotation to this object
+            transform.rotation = Quaternion.LookRotation(Target);
+        }
+        else
+        {
+            TargetPlayer();
+        }
     }
 
     void TargetPlayer()
@@ -52,6 +109,22 @@ public class ICBM : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Destroyable")
+        {
+            if (collision.gameObject.GetComponent<Factory>().enabled == true)
+            {
+                collision.gameObject.GetComponent<Factory>().Smash();
+            }
+        }
+
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            AttackPlayer = true;
+        }
     }
 }
