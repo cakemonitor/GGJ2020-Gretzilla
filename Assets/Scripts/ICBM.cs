8211854b5c00330a6dac1f3c3abfa8b478b1a1e4
harrygood619 <harrygood619@users.noreply.github.com>
2020-02-02 11:38:46 +0000

    ICBM Tracking
    Got the ICBM flying up from the centre of the earth and then at a certain point, start targetting the player

--- Assets/Scripts/ICBM.cs
+++ Assets/Scripts/ICBM.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class ICBM : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform PlayerTransform;
+
+    public float Speed;
+    public float TargettingHeight;
+
+    void Awake()
     {
-        
+        if (PlayerTransform == null)
+        {
+            PlayerController TempPlayerController = (PlayerController)FindObjectOfType(typeof(PlayerController));
+            GameObject TempPlayer = TempPlayerController.gameObject;
+            PlayerTransform = TempPlayer.transform;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (TargettingHeight != 0)
+        {
+            Move();
+            TargettingHeight -= 1;
+        }
+        else
+        {
+            TargetPlayer();
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+    }
+
+    void TargetPlayer()
+    {
+        // Determine which direction to rotate towards
+        Vector3 TargetDirection = (PlayerTransform.position - transform.position);
+        // The step size is equal to speed times frame time.
+        float SingleStep = ((Speed * 0.1f) * Time.deltaTime);
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 Target = Vector3.RotateTowards(transform.forward, TargetDirection, SingleStep, 0.0f);
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(Target);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
     }
 }
